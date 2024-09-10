5ba61f480764109672c3a501ce050d06a0ba1d79
tomas-tissera <tomas.tissera.trabajo@gmail.com>
2024-09-10 10:16:20 -0300

    se agrego simbolo de carga

--- src/components/servicioComponente/index.jsx
+++ src/components/servicioComponente/index.jsx
@@ -5,15 +5,21 @@ import styles from './ServicioComponte.module.css'; // Importar los estilos del 
 
 const ServicioComponte = () => {
   const [gastos, setGastos] = useState([]);
+  const [loading, setLoading] = useState(true); // Estado para controlar si está cargando
 
-  // Función para obtener los gastos de Firebase
   const fetchGastos = async () => {
-    const querySnapshot = await getDocs(collection(db, 'gastos'));
-    const gastosList = querySnapshot.docs.map(doc => ({
-      id: doc.id,
-      ...doc.data()
-    }));
-    setGastos(gastosList);
+    try {
+      const querySnapshot = await getDocs(collection(db, 'gastos'));
+      const gastosList = querySnapshot.docs.map(doc => ({
+        id: doc.id,
+        ...doc.data()
+      }));
+      setGastos(gastosList);
+    } catch (error) {
+      console.error("Error fetching gastos:", error);
+    } finally {
+      setLoading(false); // Deja de mostrar el loader cuando los datos están listos
+    }
   };
 
   useEffect(() => {
@@ -23,16 +29,20 @@ const ServicioComponte = () => {
   return (
     <div className={styles.servicioContainer}>
       <h2>Gastos Creados</h2>
-      <div className={styles.gastosList}>
-        {gastos.map(gasto => (
-          <div key={gasto.id} className={styles.gastoCard} style={{ borderLeft: `5px solid ${gasto.color}` }}>
-            <h3>{gasto.nombreGasto}</h3>
-            <p><strong>Monto:</strong> ${gasto.monto}</p>
-            <p><strong>Fecha a Debitar:</strong> {gasto.fecha}</p>
-            {gasto.descripcion && <p><strong>Descripción:</strong> {gasto.descripcion}</p>}
-          </div>
-        ))}
-      </div>
+      {loading ? (
+        <div className={styles.loader}></div> // Mostrar el spinner mientras carga
+      ) : (
+        <div className={styles.gastosList}>
+          {gastos.map(gasto => (
+            <div key={gasto.id} className={styles.gastoCard} style={{ borderLeft: `5px solid ${gasto.color}` }}>
+              <h3>{gasto.nombreGasto}</h3>
+              <p><strong>Monto:</strong> ${gasto.monto}</p>
+              <p><strong>Fecha a Debitar:</strong> {gasto.fecha}</p>
+              {gasto.descripcion && <p><strong>Descripción:</strong> {gasto.descripcion}</p>}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
