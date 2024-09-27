0e73847365e0a27c0c36e0f7848f0a9cada93f2c
tomas-tissera <tomas.tissera.trabajo@gmail.com>
2024-09-27 10:14:58 -0300

    se agrego que no se vea el navbar en el login

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import Layout from "./Layout";
 import Login from "./Login";
 import Signup from "./Signup";
@@ -5,35 +6,44 @@ import Profile from "./Profile";
 import Carga from "./views/carga";
 import Servicios from "./views/servicios";
 import Actualizar from "./views/Prestamos";
-import EditarG from "./views/editarG"
+import EditarG from "./views/editarG";
 import TarjetasGastos from "./views/tarjetasG";
 import EditarGastoTarjeta from "./components/EditarGastoTarjeta";
 import EditarTarjeta from "./components/EditarTarjeta";
 import EditarPrestamo from "./components/EditarPrestamo";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar";
+
 const App = () => {
   return (
     <BrowserRouter>
-    <Navbar/>
-      <Routes>
-          <Route path = "/" element = { <Layout></Layout> }>
-            <Route index element = { <Login></Login> }></Route>
-            <Route path = "/carga" element = { <Carga></Carga> } ></Route>
-            <Route path = "/servicios" element = { <Servicios></Servicios> } ></Route>
-            <Route path="/servicios/:id" element={<EditarG />} />
-            <Route path = "/prestamos" element = { <Actualizar></Actualizar> } ></Route>
-            <Route path = "/prestamos/:prestamoId" element = { <EditarPrestamo></EditarPrestamo> } ></Route>
-            <Route path = "/profile" element = { <Profile></Profile> }></Route>
-            <Route path = "/tarjetas" element = { <TarjetasGastos></TarjetasGastos> }></Route>
-            <Route path="/tarjetas/:gastoId" element={<EditarGastoTarjeta />} /> {/* Agregar esta ruta */}
-            <Route path="/tarjetas/editarTarjeta/:tarjetaId" element={<EditarTarjeta />} /> {/* Agregar esta ruta */}
-            
-
-          </Route>
-      </Routes>
+      <Main />
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
+const Main = () => {
+  const location = useLocation();
+
+  return (
+    <>
+      {/* Ocultar el Navbar si la ruta es "/" */}
+      {location.pathname !== "/" && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Login />} />
+          <Route path="/carga" element={<Carga />} />
+          <Route path="/servicios" element={<Servicios />} />
+          <Route path="/servicios/:id" element={<EditarG />} />
+          <Route path="/prestamos" element={<Actualizar />} />
+          <Route path="/prestamos/:prestamoId" element={<EditarPrestamo />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/tarjetas" element={<TarjetasGastos />} />
+          <Route path="/tarjetas/:gastoId" element={<EditarGastoTarjeta />} />
+          <Route path="/tarjetas/editarTarjeta/:tarjetaId" element={<EditarTarjeta />} />
+        </Route>
+      </Routes>
+    </>
+  );
+};
+
+export default App;
